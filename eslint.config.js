f0aa67bac13fad732ed47d70d9a461765a0d6729
IgorMouraS <igormsousa2003@gmail.com>
2024-12-03 00:56:06 -0300

    Configurações e estruturação do projeto

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier, // Adiciona Prettier para desativar regras conflitantes do ESLint
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Desativa a exigência do React no escopo
+      'prettier/prettier': 'error', // Configura Prettier como uma regra de ESLint
+    },
+  },
+];
---
+++ src/services/exchangeService.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL =
+  'https://v6.exchangerate-api.com/v6/8e0373da95fab0502608ea54/latest/';
+
+// Função para buscar as taxas de câmbio
+export const fetchExchangeRates = async (baseCurrency: string) => {
+  try {
+    const response = await axios.get(`${API_URL}${baseCurrency}`);
+    console.log('Rsponse: ' + response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar as taxas de câmbio:', error);
+    throw error;
+  }
+};
